da36d3bae59b3d5b11dfa5b216ff4a85a8599dc4
vorobeevich <valerygegelevsky@yandex.ru>
2022-10-13 22:34:08 +0300

    Add wer and cer metrics

--- hw1/asr_project_template/hw_asr/metric/utils.py
+++ hw1/asr_project_template/hw_asr/metric/utils.py
@@ -1,10 +1,13 @@
-# Don't forget to support cases when target_text == ''
+import editdistance
 
-def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+def calc_cer(target_text: str, predicted_text: str) -> float:
+    if len(target_text) == 0:
+        return 1
+    return editdistance.distance(target_text, predicted_text) / len(target_text)
 
 
-def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+def calc_wer(target_text: str, predicted_text: str) -> float:
+    splitted_target = target_text.split()
+    if len(splitted_target) == 0:
+        return 1
+    return editdistance.distance(splitted_target, predicted_text.split()) / len(splitted_target)
